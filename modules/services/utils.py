db651c6729d8c1fb88f4465fd22beafd647f9050
SpardaRia <esterhazzy@mail.ru>
2024-04-26 12:34:53 +0300

    first commit

---
+++ modules/services/utils.py
@@ -0,0 +1,81 @@
+import os
+from uuid import uuid4
+from pytils.translit import slugify
+from django.core.files.storage import FileSystemStorage
+from backend import settings
+from urllib.parse import urljoin
+from datetime import datetime
+
+
+
+def unique_slugify(instance, slug):
+
+    """
+
+    Генератор уникальных SLUG для моделей, в случае существования такого SLUG.
+
+    """
+
+    model = instance.__class__
+
+    unique_slug = slugify(slug)
+
+    while model.objects.filter(slug=unique_slug).exists():
+
+        unique_slug = f'{unique_slug}-{uuid4().hex[:8]}'
+
+    return unique_slug
+
+def get_client_ip(request):
+
+    """
+
+    Получение IP адреса
+
+    """
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+
+        ip = x_forwarded_for.split(',')[0]
+
+    else:
+
+        ip = request.META.get('REMOTE_ADDR')
+
+    return ip
+
+class CkeditorCustomStorage(FileSystemStorage):
+
+    """
+
+    Кастомное расположение для медиа файлов редактора
+
+    """
+
+    def get_folder_name(self):
+
+        return datetime.now().strftime('%Y/%m/%d')
+
+
+
+    def get_valid_name(self, name):
+
+        return name
+
+
+
+    def _save(self, name, content):
+
+        folder_name = self.get_folder_name()
+
+        name = os.path.join(folder_name, self.get_valid_name(name))
+
+        return super()._save(name, content)
+
+
+
+    location = os.path.join(settings.MEDIA_ROOT, 'uploads/')
+
+    base_url = urljoin(settings.MEDIA_URL, 'uploads/')
